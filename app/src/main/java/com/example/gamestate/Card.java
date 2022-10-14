d5b0e287bb3ad3b562f6e8da58719047a0da54b3
melemai25 <112668823+melemai25@users.noreply.github.com>
2022-10-13 21:15:16 -0700

    Update Card.java

--- app/src/main/java/com/example/gamestate/Card.java
+++ app/src/main/java/com/example/gamestate/Card.java
@@ -6,26 +6,26 @@ package com.example.gamestate;
 public class Card {
 
 
-    public Card(FACE f, SUIT s)
+    public Card(FACE f, SUIT s) //Card constructor assigns face value and suit
     {
         face = f;
         suit = s;
     }
 
-    public enum FACE
+    public enum FACE //enum for ace through king constants
     {
        ACE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, KING, QUEEN, JACK
     }
 
-    public enum SUIT
+    public enum SUIT //enum for suit constants
     {
         HEART, SPADE, DIAMOND, CLUB
     }
 
-    public FACE face;
+    public FACE face; // face and suit values initalized
     public SUIT suit;
 
-    public String toString()
+    public String toString() //toString for data of card
     {
         return suit.name() + "-" + face.name();
     }
